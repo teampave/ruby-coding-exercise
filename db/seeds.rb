e7159790b5be2cd672daf92fe2047d3fe6354dec
sammo <samrhysperry93@hotmail.co.uk>
2022-10-18 17:19:13 +0100

    Add faker gem and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product_names = ["Guitar", "Record Player", "Stereo", "MIDI Interface", ]
+
+product_names.each do |name|
+  Product.create(
+    title: name,
+    description: Faker::Lorem.sentence,
+    additional_details: Faker::Lorem.paragraph,
+    cost_cents: rand(10000),
+    quantity_in_stock: rand(200)
+  )
+end
